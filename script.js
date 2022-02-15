a68abb036bb82b523e36bb82f70bbd3d741d6410
aman amanov <amanfendamanov@gmail.com>
2022-02-16 00:24:40 +0600

    ready to 7 hourse

---
+++ script.js
@@ -0,0 +1,79 @@
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+let btn = document.getElementsByClassName('season__btn')
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", function() {
+        
+        btn[i].classList.remove('btn__active')
+        btn[i].classList.toggle('btn__active')
+
+    });
+  }
+
+  let burger__menu = document.getElementById('burger__menu')
+  let burger = document.getElementById('burger')
+  let back = document.getElementById('back')
+
+  burger.addEventListener("click", function() {
+        
+    burger__menu.style.display = "block";
+    console.log('good');
+
+})
+back.addEventListener("click", function() {
+        
+    burger__menu.style.display = "none";
+    console.log('good');
+
+})
+let slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("item");
+    let dots = document.getElementsByClassName("slider-dots_item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
